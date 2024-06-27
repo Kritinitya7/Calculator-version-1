35a4896a40446ce629b71bfffafe9cbf5071cb44
Kritinitya7 <kritinitya7@gmail.com>
2024-06-27 13:57:59 +0530

    version-1-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import "./App.css";
-
+import Display from "./components/Display";
+import styles from "./App.module.css";
+import ButtonsContainer from "./components/ButtonsContainer";
 function App() {
-  return <></>;
+  return (
+    <div className={styles.calculator}>
+      <Display></Display>
+      <ButtonsContainer></ButtonsContainer>
+    </div>
+  );
 }
 
 export default App;
